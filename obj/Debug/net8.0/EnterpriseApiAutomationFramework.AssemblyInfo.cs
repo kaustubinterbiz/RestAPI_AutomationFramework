a4e77890c695ada02c55c764cb9fdc1322d083e1
kaustub <kaustub.kundet@interbizconsulting.com>
2026-05-20 01:32:05 +0530

    added some bin and obj files

--- obj/Debug/net8.0/EnterpriseApiAutomationFramework.AssemblyInfo.cs
+++ obj/Debug/net8.0/EnterpriseApiAutomationFramework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnterpriseApiAutomationFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e9f0d48ab2738703c7a38b7776c9620c31acb76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00431113cf38698da85c80100c9b89916b69932")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnterpriseApiAutomationFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnterpriseApiAutomationFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
